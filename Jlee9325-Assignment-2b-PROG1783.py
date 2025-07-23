16a67de39169d3637577dcac5e3d31737310221c
Jlee9325 <jlee9325@conestogac.on.ca>
2025-07-23 13:56:29 -0400

    Constants-for-recepts

--- Jlee9325-Assignment-2b-PROG1783.py
+++ Jlee9325-Assignment-2b-PROG1783.py
@@ -8,3 +8,14 @@
 #  allows delivery/pickup, applies discounts and tips, and prints the receipt
 
 
+
+#these are my constants for the receipt this includes taxes, delivery, tips, etc
+
+STUDENT_DISCOUNT_RATE = 0.15
+HST_RATE = 0.13
+DELIVERY_FEE = 7.00
+FREE_DELIVERY = 25.00
+TIP_OPTIONS = {1: 1.00, 2: 2.00, 3: 5.00}
+
+
+
