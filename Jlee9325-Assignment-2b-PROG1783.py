1ca63eb60097d78ffc8ba3dc4c57314bb12c21c0
Jlee9325 <jlee9325@conestogac.on.ca>
2025-07-23 13:45:39 -0400

    Starting-of-assignment-of-2b

---
+++ Jlee9325-Assignment-2b-PROG1783.py
@@ -0,0 +1,10 @@
+# Name:                 Jlee9325-assignment2b.py
+# Author:               Justin Lee
+# Date Created:         July 23th, 2025
+# Date Last Modified:   July 23rd, 2025
+
+# Purpose:
+#  Expand on Arnolds's eats - collect customers orders
+#  allows delivery/pickup, applies discounts and tips, and prints the receipt
+
+
